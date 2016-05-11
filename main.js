aedc4981336215f9ff7edf02d1041b073a14843a
Areii1 <ari.jaaskelainen1@gmail.com>
2016-05-11 21:17:40 +0300

    Refactored nextImage function to handle image addresses dynamically.

--- main.js
+++ main.js
@@ -1,23 +1,36 @@
-var currentImageOrderNumber = 0;
-document.getElementById('img1').style.display = 'block';
-document.getElementById('img2').style.display = 'none';
-document.getElementById('img3').style.display = 'none';
+var currentImageOrderNumber = 1;
+var liElementArray = document.getElementById('imgList').getElementsByTagName('li');
+var liLength = liElementArray.length;
+console.log(liLength);
+
+renderFirstImage();
 
 document.getElementById("nextButton").addEventListener("click", function(){
-    nextImage(currentImageOrderNumber);
-    currentImageOrderNumber++;
+    if (currentImageOrderNumber == liLength) {
+		renderFirstImage();
+		currentImageOrderNumber = 1;
+	}
+	else {
+		nextImage(currentImageOrderNumber);
+   		currentImageOrderNumber++;	
+	}
 });
 
+function renderFirstImage() {
+	document.getElementById('img1').style.display = 'block';
+	for (var i = 2; i <= liLength; i++) {
+		document.getElementById('img' + i).style.display = 'none';
+	}
+
+}
+
 function nextImage(currentImageOrderNumber) {
-	if (currentImageOrderNumber == 0) {
-		document.getElementById('img1').style.display = 'none';
-		document.getElementById('img2').style.display = 'block';
-		document.getElementById('img3').style.display = 'none';
+	for (var i = 1; i <= liLength; i++) {
+		if (i == currentImageOrderNumber + 1) {
+			document.getElementById('img' + i).style.display = 'block';
+		}
+		else {
+			document.getElementById('img' + i).style.display = 'none';
+		}
 	}
-	else if (currentImageOrderNumber == 1) {
-		document.getElementById('img1').style.display = 'none';
-		document.getElementById('img2').style.display = 'none';
-		document.getElementById('img3').style.display = 'block';
-	}
-	console.log(currentImageOrderNumber);
 }
